cac4d24893eac2e706fb9d58e3caf3db590819fe
Joaquin Franco <chino.franco@gmail.com>
2024-07-30 00:48:52 +0900

    fix: unittesting for json export

--- tests/test_template_docker.py
+++ tests/test_template_docker.py
@@ -1,3 +1,4 @@
+import json
 import os
 from pathlib import Path
 
@@ -5,6 +6,20 @@ from pytest import LogCaptureFixture, MonkeyPatch
 
 from tests.conftest import TestRunner
 from tests.helpers import assert_files_created, assert_lines_in_file
+from vulcanbox.models import DockerImage
+
+
+def test_docker_object_json() -> None:
+    expected_json = {
+        "name": "test.Dockerfile",
+        "ports": [5000, 5050],
+        "tag": None,
+        "context": {"foo": "bar"},
+    }
+    test_image = DockerImage(
+        name="test.Dockerfile", ports=[5000, 5050], context={"foo": "bar"}
+    )
+    assert test_image.json() == expected_json
 
 
 def test_template_new_image_sane(
@@ -109,3 +124,43 @@ def test_template_new_image_file_already_exists(
 
     assert result.exit_code == 2
     assert f"Dockerfile already exists: {str(existing_file)}" in caplog.text
+
+
+def test_template_new_image_json_exported(
+    tmp_path: Path,
+    monkeypatch: MonkeyPatch,
+    runner: TestRunner,
+    caplog: LogCaptureFixture,
+) -> None:
+    monkeypatch.chdir(tmp_path)
+    dockerfile_name = "test.Dockerfile"
+    exported_json_file = "vulcanbox-test-ubuntu-20.04.json"
+    expected_files = [dockerfile_name, exported_json_file]
+
+    result = runner.run_cli(
+        [
+            "-vv",
+            "docker",
+            "new",
+            "--name",
+            dockerfile_name,
+            "--base",
+            "ubuntu:20.04",
+            "--expose",
+            "5050",
+            "--export-config",
+        ]
+    )
+
+    assert result.exit_code == 0
+    assert_files_created(str(tmp_path), expected_files)
+    test_json_file = os.path.join(str(tmp_path), exported_json_file)
+    with open(test_json_file, "r") as config_file:
+        expected_json = {
+            "name": "test.Dockerfile",
+            "ports": [5050],
+            "tag": None,
+            "context": {"base_image": "ubuntu:20.04", "ports": [5050]},
+        }
+        json_data = json.load(config_file)
+        assert json_data == expected_json
